d104e86afb45ff5883ea07f5ddb3391bd05c520f
Ben Burns <803016+benjamincburns@users.noreply.github.com>
2023-04-12 04:18:38 +0000

    Make rust-carball build on non-win OSes
    In order to avoid a breaking change for windows users, building on
    non-win OSes requires passing the `--no-default-features` flag.

--- src/analysis/mod.rs
+++ src/analysis/mod.rs
@@ -1,10 +1,15 @@
 pub mod analyzer;
+
+#[cfg(feature = "ball_prediction")]
 pub mod ball_prediction;
+
 pub mod gameplay_period;
 pub mod hit;
 pub mod stats;
 
 pub use self::analyzer::*;
+
+#[cfg(feature = "ball_prediction")]
 pub use self::ball_prediction::*;
 pub use self::gameplay_period::*;
 pub use self::hit::*;
